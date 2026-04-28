86a94a7cd3b1d4942f48e47d79efbf68d5f987ee
Gray23rd <lovewasho@gmail.com>
2026-04-28 23:57:38 +0300

    Connect to hosted ML service

--- ml/predict.py
+++ ml/predict.py
@@ -198,4 +198,4 @@ if __name__ == '__main__':
     print("="*50)
     print(f"Running on: http://localhost:5001")
     print("="*50)
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
